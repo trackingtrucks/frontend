874f5e355f9d822622754d03c94179997eee844e
tomipodo <70075163+tomipodo@users.noreply.github.com>
2021-04-19 09:56:33 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ORT, no lo entenderias.
+          ORT, no lo entenderiasa.
         </p>
         
       </header>
