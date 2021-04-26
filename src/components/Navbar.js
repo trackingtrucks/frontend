8e383a740a65ca450ca985152bf0955ed696b609
fedehilsen <71460515+fedehilsen@users.noreply.github.com>
2021-04-26 10:35:58 -0300

    mapa padre!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ export class NavBar extends Component {
                             <Nav.Link href="#link">Link</Nav.Link>
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">No lo entenderias</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
