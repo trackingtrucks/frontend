78f4a8210be4ae0be2e1c9a229dcd371a78e6ea2
fedehilsen <71460515+fedehilsen@users.noreply.github.com>
2021-07-15 10:34:37 -0300

    intento mapa

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,45 @@
 import React, { Component } from 'react'
-import {Alert} from 'react-bootstrap'
+import { Alert } from 'react-bootstrap'
 //import Card from './Objects/card.js'
 import axios from 'axios'
 import Config from '../Config'
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
 let token = localStorage.getItem("accessToken");
+
 export class Home extends Component {
-    
+
     componentDidMount = () => {
         this.getCarros();
 
     }
-    getCarros = async () =>{
+    getCarros = async () => {
         try {
             const perfil = await axios.get(
                 Config.API_URL + "/company",
                 {
                     headers: {
-                        "x-access-token": token 
+                        "x-access-token": token
                     }
                 }
             )
             console.log(perfil.data)
             localStorage.setItem("vehiculos", JSON.stringify(perfil.data.vehiculos))
         }
-        catch(error){
+        catch (error) {
             console.log(error.response.data.message)
         }
     }
 
     render() {
-        
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZmVkZWhpbHNlbiIsImEiOiJja3I0dzRkZTEyenhzMm9vOGs0YmdsM2ozIn0.jTC_nHxqAkIF_DODrdBgPA';
+        var map = new mapboxgl.Map({
+            container: "mapa1",
+            style: 'mapbox://styles/fedehilsen/ckr4w7vz10fjz18se2at1fjgk'
+        });
         return (
             <>
-                <div className="mapa">
-                    <iframe className="in-flex" title="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15688920.90362076!2d-69.95631117490196!3d-36.1692793844697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccaf5f5fdc667%3A0x3d2f77992af00fa8!2sArgentina!5e0!3m2!1sen!2sar!4v1619443032172!5m2!1sen!2sar"></iframe>
+                <div id="mapa1" className="mapa">
+                {map}
                 </div>
                 <Alert variant="primary">
                     {localStorage.getItem("vehiculos")}
