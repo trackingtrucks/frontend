15362c374ef3c5473df47c0f60ae27260e02bba6
fedehilsen <71460515+fedehilsen@users.noreply.github.com>
2021-08-12 10:40:35 -0300

    lista con modals

--- src/Components/Turnos/TurnosCard.js
+++ src/Components/Turnos/TurnosCard.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { ListGroup } from 'react-bootstrap';
+import { React, useState } from 'react'
+import { ListGroup, Button, Modal } from 'react-bootstrap';
 
 function TurnosList({ turno, accessToken, api, getCarros }) {
     // codigoDeTurno: "abd5532h45e22s"
@@ -8,9 +8,39 @@ function TurnosList({ turno, accessToken, api, getCarros }) {
     // fechaYhora: "2021-07-08T21:00:00.000Z"
     // nombreVendedor: "ventasLuca"
     // _id: "60e4aa9690042119508f172f"
+    // condicion: "Asignado/No asignado"
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <div>
-            <ListGroup.Item>{turno.nombreVendedor} {turno.fechaYhora}</ListGroup.Item>
+            <ListGroup.Item>
+                <b>
+                    {turno.nombreVendedor}
+                </b>
+                { } {turno.codigoOrdenDeCompra} {<Button variant="outline-primary" onClick={handleShow}>
+                    Asignar turno
+                </Button>}
+            </ListGroup.Item>
+
+
+
+            <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
--- src/Components/Turnos/TurnosContainer.js
+++ src/Components/Turnos/TurnosContainer.js
@@ -3,9 +3,11 @@ import {ListGroup} from 'react-bootstrap';
 import TurnosCard from './TurnosCard'
 
 function TurnosContainer({turnos}) {
+    console.log(turnos);
     return (
             <ListGroup>
             {turnos && turnos.map((turno) => {
+                if(turno.condicion === "Asignado") return null;
                 return (
                     <TurnosCard turno={turno} key={turno._id} api={Api}/>)
             })}
