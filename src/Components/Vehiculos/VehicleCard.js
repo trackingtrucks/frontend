286e591caf61fc137c8187ab84c5a85a307d4c33
fedehilsen <71460515+fedehilsen@users.noreply.github.com>
2021-09-14 01:44:58 -0300

    a(chica)

--- src/Components/Vehiculos/VehicleCard.js
+++ src/Components/Vehiculos/VehicleCard.js
@@ -1,19 +1,20 @@
 import { React, useState, useContext } from 'react'
-import { Col, Card, Modal, Button } from 'react-bootstrap';
+import { Col, Card, Modal, Container, Tabs, Tab } from 'react-bootstrap';
 import datosSocket from "./../../socket.io"
 
 function VehicleList({ vehiculo, accessToken, api, getCarros }) {
     const [show, setShow] = useState(false);
-    const datos  = useContext(datosSocket);
+    const datos = useContext(datosSocket);
 
     function handleShow() {
         setShow(true);
     }
 
+
     return (
         <div>
             <Col>
-                <Card border='secondary'>
+                <Card border='secondary' onClick={() => handleShow()}>
                     <Card.Body>
                         <Card.Title>{vehiculo.patente}</Card.Title>
                         <Card.Text>{vehiculo.alertas.map(element => {
@@ -33,17 +34,51 @@ function VehicleList({ vehiculo, accessToken, api, getCarros }) {
                             <ProgressBar animated variant="danger" now={10} key={3} />
                         </ProgressBar> */}
                         {/* <Button onClick={handleShow}></Button> */}
-                        <Button variant="outline-primary" style={{ marginRigth: '10px', marginLeft: '10px', marginBottom: '5px' }} onClick={() => handleShow()}>
+                        {/* <Button variant="outline-primary" style={{ marginRigth: '10px', marginLeft: '10px', marginBottom: '5px' }} onClick={() => handleShow()}>
                             Mas datos
-                        </Button>
+                        </Button> */}
                     </Card.Body>
                 </Card>
             </Col>
             <Modal show={show} size={"fullscreen"} onHide={() => setShow(false)}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>{datos}</Modal.Body>
+                <Modal.Body>
+                    <br /><br />
+                    <Container className="center">
+                        <b>vehiculo {vehiculo.patente}</b>
+
+                    </Container>
+                    <br /><br />
+                    <Container className="center">
+                        Conductor actual
+
+                    </Container>
+                    <br /><br />
+                    <Container className="center">
+                        {/* {(()=> {
+                            if (vehiculo.conductorActualid === null) {
+                                return ("no hay ningun conductor asignado al vehiculo")
+                            }
+                            else {
+                                return (vehiculo.conductorActual.id)
+                            }
+                        })()} */}
+                    </Container>
+                    <Container>
+                        Informacion:
+                        <br />
+                        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+                            <Tab eventKey="Nivel de nafta" title="Nivel de nafta">
+                                {datos}
+                            </Tab>
+                            <Tab eventKey="Velocidad" title="Velocidad">
+                            </Tab>
+                            <Tab eventKey="Revoluciones por minuto" title="Revoluciones por minuto">
+                            </Tab>
+                            <Tab eventKey="Temperatura del liquido refrigerante" title="Temperatura del liquido refrigerante">
+                            </Tab>
+                        </Tabs>
+                    </Container>
+                </Modal.Body>
             </Modal>
         </div>
     )
